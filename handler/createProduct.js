fd881ade6eb473240e62c38c44ae4f0d80560785
MarioPazmino <mrpazmino2@espe.edu.ec>
2024-12-15 00:31:16 -0500

    Update createProduct.js
    Subido Recien

--- handler/createProduct.js
+++ handler/createProduct.js
@@ -1,11 +1,9 @@
 const AWS = require('aws-sdk');
 const { v4 } = require('uuid');
-
 exports.createProduct = async (event) => {
   const dynamodb = new AWS.DynamoDB.DocumentClient();
   const { name, description, price } = JSON.parse(event.body);
   console.log("Datos recibidos:", { name, description, price });
-
   const createdAt = new Date();
   const ProductID = v4();
 
@@ -16,14 +14,12 @@ exports.createProduct = async (event) => {
     Price: price,
     CreatedAt: createdAt.toISOString(),
   };
-
   try {
     await dynamodb
       .put({
         TableName: "Grupo5TableNew",  
         Item: newProduct,
-      })
-      .promise();
+      }).promise();
 
     return {
       statusCode: 200,
@@ -31,8 +27,7 @@ exports.createProduct = async (event) => {
     };
   } catch (error) {
     console.error("Error al crear el producto:", error);
-    return {
-      statusCode: 500,
+    return {statusCode: 500,
       body: JSON.stringify({ message: "Error interno del servidor", error: error.message }),
     };
   }
